a773ffa5338ebda9418fbc2f4f2b91461a8f031b
Hamzah Al Hariri <gmhamalh@gotamedia.se>
2022-05-16 11:49:24 +0200

    feat: added onValueChange callback to Textarea component

--- src/lib/components/Textarea/Textarea.tsx
+++ src/lib/components/Textarea/Textarea.tsx
@@ -1,20 +1,37 @@
-import { forwardRef } from 'react'
+import {
+    forwardRef,
+    useCallback
+} from 'react'
 
 import * as Styled from './style'
 import type * as Types from './types'
+import type { ChangeEventHandler } from 'react'
 
 const Textarea: Types.TextareaComponent = forwardRef((props, ref) => {
     const {
         size = 'normal',
         variant = 'primary',
+        onChange,
+        onValueChange,
         ...DOMProps
     } = props
+
+    const handleOnChange: ChangeEventHandler<HTMLTextAreaElement> = useCallback((event) => {
+        if (typeof onChange === 'function') {
+            onChange(event)
+        }
+
+        if (typeof onValueChange === 'function') {
+            onValueChange(event.target.value)
+        }
+    }, [onChange, onValueChange])
 
     return (
         <Styled.Textarea
             ref={ref}
             $size={size}
             $variant={variant}
+            onChange={handleOnChange}
             {...DOMProps}
         />
     )
