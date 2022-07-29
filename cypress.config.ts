734cc6967023e9c30ff9dc3e6f776b28355fd975
Hamzah Al Hariri <gmhamalh@gotamedia.se>
2022-07-29 10:50:00 +0200

    chore(test): Updated cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
     retries: 2,
     viewportWidth: 1000,
     viewportHeight: 660,
+    video: false,
+    screenshotOnRunFailure: false,
     component: {
         devServer: {
             framework: 'create-react-app',
