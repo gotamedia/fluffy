f26e0adac4af04a9c192f55b883ff1874f25950a
Hamzah Al Hariri <gmhamalh@gotamedia.se>
2022-08-08 14:58:48 +0200

    fix(style): Fixed Container background-color

--- src/components/Container/style.ts
+++ src/components/Container/style.ts
@@ -7,6 +7,7 @@ import type * as Types from './types'
 const Wrapper = styled.div<{ $backdrop: Types.ContainerProps['backdrop'] }>`
     ${({ $backdrop }) => backdrops[$backdrop || 'medium']};
     border-radius: 5px;
+    background-color: white;
 `
 
 export {
