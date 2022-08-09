eb2411592e4a6d513fdc56d302f3405dce2257c4
Hamzah Al Hariri <gmhamalh@gotamedia.se>
2022-08-09 13:25:28 +0200

    fix: Minor fixes and cleanup

--- src/components/Dropdown/style.ts
+++ src/components/Dropdown/style.ts
@@ -1,12 +1,6 @@
 import styled from 'styled-components'
 
 import IconComponent, { Icons } from '../Icon'
-import PopoverComponent from '../Popover'
-import ContainerComponent from '../Container'
-
-const Wrapper = styled.div`
-    
-`
 
 const Icon = styled(IconComponent).attrs(({ $isOpen } : { $isOpen: boolean }) => {
     return {
@@ -16,18 +10,6 @@ const Icon = styled(IconComponent).attrs(({ $isOpen } : { $isOpen: boolean }) =>
 
 `
 
-const Popover = styled(PopoverComponent)`
-
-`
-
-const Container = styled(ContainerComponent)`
-    overflow: auto;
-    height: 100%;
-`
-
 export {
-    Wrapper,
-    Icon,
-    Popover,
-    Container
+    Icon
 }
