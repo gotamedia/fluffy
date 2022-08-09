9db70ef730d3e2da5b8e89693420482652a9075c
Hamzah Al Hariri <gmhamalh@gotamedia.se>
2022-08-09 13:24:34 +0200

    feat!: Added "uuid" lib and exposed as a package

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 export * as Polished from 'polished'
 export * as ReactIntersectionObserver from 'react-intersection-observer'
 export { default as ScrollIntoViewIfNeeded } from 'scroll-into-view-if-needed'
+export * from 'uuid'
 
 // <==== Packages
 
