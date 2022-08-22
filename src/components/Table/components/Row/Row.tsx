b2a684b2620debe346321d6d5d08503cc55449a7
Diego Edward Perez <diego.perez@gotamedia.se>
2022-08-22 11:29:49 +0200

    feat(TableRow): Added the abililty for the parent to listen for mouse enter and mouse leave events

--- src/components/Table/components/Row/Row.tsx
+++ src/components/Table/components/Row/Row.tsx
@@ -1,49 +1,63 @@
-import { forwardRef, useCallback, useEffect, useMemo, useState } from "react"
+import { forwardRef, MouseEventHandler, useCallback, useEffect, useMemo, useState } from "react"
 import { TableContext, useTableContext } from "../context"
 import { TableElements } from "../types"
 import * as Style from "./style"
 import * as Types from "./types"
 
-const TableRow: Types.TableRow = forwardRef(({ children, onHover, hover, ...rest }, ref) => {
-    const [isHover, setIsHover] = useState<boolean>()
-    const { type, state } = useTableContext()
-    const elementType: TableElements = "tr"
-    const context = useMemo(
-        () => ({
-            type: elementType,
-            state: state,
-            parentType: type,
-            parentState: state,
-        }),
-        [elementType, type, state]
-    )
+const TableRow: Types.TableRow = forwardRef(
+    ({ children, onMouseEnter, onMouseLeave, onHover, hover, ...rest }, ref) => {
+        const [isHover, setIsHover] = useState<boolean>()
+        const { type, state } = useTableContext()
+        const elementType: TableElements = "tr"
+        const context = useMemo(
+            () => ({
+                type: elementType,
+                state: state,
+                parentType: type,
+                parentState: state,
+            }),
+            [elementType, type, state]
+        )
 
-    useEffect(() => {
-        if (onHover && isHover !== undefined) {
-            onHover(isHover)
-        }
-    }, [onHover, isHover])
+        useEffect(() => {
+            if (onHover && isHover !== undefined) {
+                onHover(isHover)
+            }
+        }, [onHover, isHover])
 
-    const onMouseEnter = useCallback(() => {
-        setIsHover(true)
-    }, [])
+        const handleOnMouseEnter = useCallback<MouseEventHandler<HTMLTableRowElement>>(
+            (event) => {
+                setIsHover(true)
+                if (onMouseEnter) {
+                    onMouseEnter(event)
+                }
+            },
+            [onMouseEnter]
+        )
 
-    const onMouseLeave = useCallback(() => {
-        setIsHover(false)
-    }, [])
+        const handleOnMouseLeave = useCallback<MouseEventHandler<HTMLTableRowElement>>(
+            (event) => {
+                setIsHover(false)
+                if (onMouseLeave) {
+                    onMouseLeave(event)
+                }
+            },
+            [onMouseLeave]
+        )
 
-    return (
-        <TableContext.Provider value={context}>
-            <Style.Row
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-                hover={hover}
-                ref={ref}
-                {...rest}
-            >
-                {children}
-            </Style.Row>
-        </TableContext.Provider>
-    )
-})
+        return (
+            <TableContext.Provider value={context}>
+                <Style.Row
+                    onMouseEnter={handleOnMouseEnter}
+                    onMouseLeave={handleOnMouseLeave}
+                    hover={hover}
+                    ref={ref}
+                    {...rest}
+                >
+                    {children}
+                </Style.Row>
+            </TableContext.Provider>
+        )
+    }
+)
 export default TableRow
