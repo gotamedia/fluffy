16f5d212b2cc8f797d2c435665cd1c3271c85c11
Hamzah Al Hariri <gmhamalh@gotamedia.se>
2022-08-22 10:26:46 +0200

    feat!: Added useMeasure hook

---
+++ src/hooks/useMeasure/useMeasure.ts
@@ -0,0 +1,63 @@
+import {
+    useRef,
+    useState,
+    useCallback
+} from 'react'
+
+import useIsomorphicLayoutEffect from '../useIsomorphicLayoutEffect'
+
+import type * as Types from './types'
+
+const defaultRect = {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
+}
+
+const useMeasure: Types.UseMeasure = (target) => {
+    const observerRef = useRef<ResizeObserver>()
+
+    const [rect, setRect] = useState<Types.UseMeasureRect>(() => {
+        if (target) {
+            return target.getBoundingClientRect()
+        } else {
+            return defaultRect
+        }
+    })
+
+    const handleOnResize = useCallback((entries: ResizeObserverEntry[]) => {
+        requestAnimationFrame(() => {
+            if (Array.isArray(entries) && entries.length) {
+                if (entries[0]) {
+                    setRect(entries[0].contentRect)
+                }
+            }
+        })
+    }, [])
+
+    useIsomorphicLayoutEffect(() => {
+        let observer = observerRef.current
+
+        if (target) {
+            if (!observer) {
+                observerRef.current = new ResizeObserver(handleOnResize)
+                observer = observerRef.current
+            }
+
+            observer.observe(target)
+        }
+
+        return () => {
+            observer?.disconnect()
+        }
+    }, [target, handleOnResize])
+
+    return rect
+}
+
+export default useMeasure
