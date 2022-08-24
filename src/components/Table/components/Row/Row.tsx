1092ad127ee3efced35956af80292a0fd1eafe5e
Diego Edward Perez <diego.perez@gotamedia.se>
2022-08-24 10:53:10 +0200

    feat(Table): Implemented Table row Head and Collapsible

--- src/components/Table/components/Row/Row.tsx
+++ src/components/Table/components/Row/Row.tsx
@@ -1,12 +1,33 @@
-import { forwardRef, MouseEventHandler, useCallback, useEffect, useMemo, useState } from "react"
+import {
+    Children,
+    isValidElement,
+    forwardRef,
+    MouseEventHandler,
+    useCallback,
+    useEffect,
+    useMemo,
+    useState,
+} from "react"
+
+import Icon, { Icons } from "@components/Icon"
+import Collapsible from "@components/Collapsible"
+
 import { TableContext, useTableContext } from "../context"
 import { TableElements } from "../types"
 import * as Style from "./style"
 import * as Types from "./types"
 
-const TableRow: Types.TableRow = forwardRef(
-    ({ children, onMouseEnter, onMouseLeave, onHover, hover, ...rest }, ref) => {
+const TableRow: Types.TableRow = forwardRef(({
+    children,
+    hiddenElement,
+    onHover,
+    onClick,
+    onMouseEnter,
+    onMouseLeave,
+    ...rest
+}, ref) => {
         const [isHover, setIsHover] = useState<boolean>()
+        const [isOpen, setIsOpen] = useState<boolean>()
         const { type, state } = useTableContext()
         const elementType: TableElements = "tr"
         const context = useMemo(
@@ -18,6 +39,13 @@ const TableRow: Types.TableRow = forwardRef(
             }),
             [elementType, type, state]
         )
+
+        const collapsible = state.collapsible
+        const findRowType = [
+            !collapsible && Types.TableRowEnum.Default,
+            collapsible && type === "thead" && Types.TableRowEnum.THeadCollapsible,
+            collapsible && type === "tbody" && isValidElement(hiddenElement) && Types.TableRowEnum.TBodyCollapsible,
+        ].find(Boolean)
 
         useEffect(() => {
             if (onHover && isHover !== undefined) {
@@ -45,19 +73,82 @@ const TableRow: Types.TableRow = forwardRef(
             [onMouseLeave]
         )
 
-        return (
-            <TableContext.Provider value={context}>
-                <Style.Row
-                    onMouseEnter={handleOnMouseEnter}
-                    onMouseLeave={handleOnMouseLeave}
-                    hover={hover}
-                    ref={ref}
-                    {...rest}
-                >
-                    {children}
-                </Style.Row>
-            </TableContext.Provider>
+        const handleCollapsibleRowOnClick = useCallback<MouseEventHandler<HTMLTableRowElement>>(
+            (event) => {
+                setIsOpen(!isOpen)
+                if (onClick) {
+                    onClick(event)
+                }
+            },
+            [isOpen, onClick]
         )
+
+        switch (findRowType) {
+            case Types.TableRowEnum.Default: {
+                return (
+                    <TableContext.Provider value={context}>
+                        <Style.Row
+                            ref={ref}
+                            onClick={onClick}
+                            onMouseEnter={handleOnMouseEnter}
+                            onMouseLeave={handleOnMouseLeave}
+                            {...rest}
+                        >
+                            {children}
+                        </Style.Row>
+                    </TableContext.Provider>
+                )
+            }
+            case Types.TableRowEnum.THeadCollapsible: {
+                return (
+                    <TableContext.Provider value={context}>
+                        <Style.HeadRow
+                            ref={ref}
+                            onClick={onClick}
+                            onMouseEnter={handleOnMouseEnter}
+                            onMouseLeave={handleOnMouseLeave}
+                            {...rest}
+                        >
+                            {children}
+                            <Style.CollapsibleEmptyCell style={{ width: "5%" }} />
+                        </Style.HeadRow>
+                    </TableContext.Provider>
+                )
+            }
+            case Types.TableRowEnum.TBodyCollapsible: {
+                const childrenCount = Children.count(children) + 1
+
+                return (
+                    <TableContext.Provider value={context}>
+                        <Style.CollapsibleRow
+                            ref={ref}
+                            $active={isOpen}
+                            onMouseEnter={handleOnMouseEnter}
+                            onMouseLeave={handleOnMouseLeave}
+                            onClick={handleCollapsibleRowOnClick}
+                            {...rest}
+                        >
+                            {children}
+                            <Style.CollapsibleIconCell>
+                                <Icon
+                                    size={"small"}
+                                    color={isOpen ? "white" : undefined}
+                                    icon={isOpen ? Icons.ArrowUp : Icons.ArrowDown}
+                                />
+                            </Style.CollapsibleIconCell>
+                        </Style.CollapsibleRow>
+                        <Style.Row>
+                            <Style.CollapsibleCell colSpan={childrenCount}>
+                                <Collapsible isOpen={isOpen}>{hiddenElement}</Collapsible>
+                            </Style.CollapsibleCell>
+                        </Style.Row>
+                    </TableContext.Provider>
+                )
+            }
+            default: {
+                return null
+            }
+        }
     }
 )
 export default TableRow
--- src/components/Table/components/Row/types.ts
+++ src/components/Table/components/Row/types.ts
@@ -1,11 +1,37 @@
-import { DetailedHTMLProps, ForwardRefExoticComponent, HTMLAttributes, RefAttributes } from "react"
+import {
+    ReactNode,
+    MouseEventHandler,
+    DetailedHTMLProps,
+    ForwardRefExoticComponent,
+    HTMLAttributes,
+    RefAttributes
+} from "react"
 
-interface TableRowProps
-    extends DetailedHTMLProps<HTMLAttributes<HTMLTableRowElement>, HTMLTableRowElement> {
+enum TableRowEnum {
+    Default = "Default",
+    TBodyCollapsible = "TBodyCollapsible",
+    THeadCollapsible = "THeadCollapsible",
+}
+
+interface TableRowCommonProps extends DetailedHTMLProps<HTMLAttributes<HTMLTableRowElement>, HTMLTableRowElement> {
     hover?: boolean
     onHover?: (isHover: boolean) => void
 }
 
+interface TableRowDefaultProps {
+    hiddenElement?: never
+}
+
+interface TableRowCollapsibleProps {
+    hiddenElement: ReactNode
+    onClick?: MouseEventHandler<HTMLTableRowElement>
+}
+
+type TableRowConditionalProps = TableRowDefaultProps | TableRowCollapsibleProps
+
+type TableRowProps = TableRowCommonProps & TableRowConditionalProps
+
 type TableRow = ForwardRefExoticComponent<TableRowProps & RefAttributes<HTMLTableRowElement>>
 
+export { TableRowEnum }
 export type { TableRow }
