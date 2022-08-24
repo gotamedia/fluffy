a7e221d6fc99c29a76b1df09f57e7865102edf59
Diego Edward Perez <diego.perez@gotamedia.se>
2022-08-24 10:45:42 +0200

    style(Table): Implemented collapsible row styles

--- src/components/Table/components/Row/style.ts
+++ src/components/Table/components/Row/style.ts
@@ -1,9 +1,48 @@
 import styled from "styled-components"
+import Cell from "../Cell"
 
-const Row = styled.tr<{ hover?: boolean }>`
-    width: 100%;
+const Row = styled.tr`
+    color: black;
+    color: ${({ theme }) => theme.colors.grey[0]};
+    font-size: ${({ theme }) => theme.fontSizes[1]}px;
+    line-height: ${({ theme }) => theme.lineHeights[1]};
+    font-family: ${({ theme }) => theme.fonts.generic[5]};
+    background-color: ${({ theme }) => theme.colors.white};
     border: ${({ theme }) => `1px solid ${theme.colors.grey[4]}`};
-    ${({ hover, theme }) =>
-        hover && `&:hover { background-color: ${theme.colors.grey[5]}; cursor: pointer; }`}
 `
-export { Row }
+
+const HeadRow = styled(Row)`
+    color: ${({ theme }) => theme.colors.grey[1]};
+    font-family: ${({ theme }) => theme.fonts.generic[4]};
+    background-color: ${({ theme }) => theme.colors.grey[5]};
+`
+
+const CollapsibleRow = styled(Row)<{ $active?: boolean }>`
+    background-color: ${({ $active }) => $active && "black"}};
+    color: ${({ theme, $active }) => ($active ? theme.colors.white : "black")}};
+
+    &:hover {
+        cursor: pointer;
+        background-color: ${({ $active, theme }) => !$active && theme.colors.grey[5]};
+    }
+`
+
+const CollapsibleEmptyCell = styled(Cell)`
+    width: 5%;
+`
+
+const CollapsibleIconCell = styled(Cell)`
+    text-align: right;
+`
+
+const CollapsibleCell = styled(Cell)`
+    padding: 0px;
+`
+export {
+    Row,
+    HeadRow,
+    CollapsibleRow,
+    CollapsibleIconCell,
+    CollapsibleCell,
+    CollapsibleEmptyCell,
+}
