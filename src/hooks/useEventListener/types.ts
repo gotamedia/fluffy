5d091ee4ef5ed237643fcfd7e2e24f7663a0943a
Diego Edward Perez <diego.perez@gotamedia.se>
2022-12-02 14:56:28 +0100

    chore: introduce an event handler type for the UseEventListener hook

--- src/hooks/useEventListener/types.ts
+++ src/hooks/useEventListener/types.ts
@@ -1,21 +1,25 @@
-/* eslint-disable no-undef */
 import type { RefObject } from 'react'
+
+import type { EventHandler, MouseEvent, TouchEvent } from 'react'
+
+type Handler<E> = (event: E) => void
 
 type UseEventListenerProps<
     Window extends keyof WindowEventMap,
     Element extends keyof HTMLElementEventMap,
     Media extends keyof MediaQueryListEventMap,
     Document extends keyof DocumentEventMap,
-    T extends HTMLElement | MediaQueryList | void = void
+    T extends Element | MediaQueryList | void = void
 > = (
     eventName: Window | Element | Media,
-    handler: (
-        event: WindowEventMap[Window]
-        | HTMLElementEventMap[Element]
-        | MediaQueryListEventMap[Media]
-        | DocumentEventMap[Document]
-        | Event
-    ) => void,
+    handler:  Handler<WindowEventMap[Window]>
+        | Handler<HTMLElementEventMap[Element]>
+        | Handler<MediaQueryListEventMap[Media]>
+        | Handler<DocumentEventMap[Document]>
+        | Handler<Event>
+        | Handler<TouchEvent>
+        | Handler<MouseEvent>
+        | EventHandler<MouseEvent>,
     element?: RefObject<T>,
     options?: boolean | AddEventListenerOptions
 ) => void
